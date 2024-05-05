ba768eb733015fdf0f1345b8805bb44183572c7d
Tobias Melen <tobias.melen@gmail.com>
2024-05-05 18:33:25 +0200

    Add controls as input for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/sunset-boat/",
   plugins: [react(), basicSsl()],
   assetsInclude: ["**/*.gltf", "**/*.hdr", "**/*.glb"],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "controls/index.html"),
+      },
+    },
+  },
 });
